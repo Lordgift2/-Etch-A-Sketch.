93c8ec88e44fe5fb7acbfa0ee9e6916748c7d2f0
Lordgift2 <Adewunmimathew@gmail.com>
2024-01-19 07:01:29 +0100

    A sketchpad with  display of width:500 and height 500 and grid of 16rowsand column and each grid size ifr was created on  the js file

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
- let color= "black"
-multipleContainer=(size)=>{
-let container = document.querySelector('.container');
-let squares = container.querySelectorAll("div");
-squares.forEach((div)=> div.remove());
-container.style.gridTemplateColumns = `repeat(${size}),1fr)`;
-container.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+ 
+    let sketchBoard = document.querySelector(".sketchBoard");
+    sketchBoard.style.gridTemplateColumns = 'repeat(16,1fr)';
+    sketchBoard.style.gridTemplateRows = 'repeat(16,1fr)';
+    console.log(sketchBoard);
 
-  let amount = size * size;
-  for (let i = 0; i < amount; i++) {
-    let square = document.createElement("div");
-    square.classList.add("cell");
-    container.style.backgroundColor = "#e0e0e0";
-    //Set up “hover” effect so the divs change color using JavaScript
-    square.addEventListener("mouseover", colorSquare);
-    container.insertAdjacentElement("beforeend", square);
-  }
-}
-colorSquare=()=>{
-    color === "random" ? this.style.backgroundColor=`hsl(${Math.random()*360},100%, 50%)`:this.style.backgroundColor = color;
-}
-multipleContainer(16);
