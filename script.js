42efcfc6a71f29e51dec9f7ea8c78fe626f4f7f1
Lordgift2 <Adewunmimathew@gmail.com>
2024-01-23 03:07:32 +0100

    an id is added to the form element in the html, In the js a function is addede so the grid could take any size

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
- 
+function porpulateBoard(size){
     let sketchBoard = document.querySelector(".sketchBoard");
-    sketchBoard.style.gridTemplateColumns = 'repeat(16,1fr)';
-    sketchBoard.style.gridTemplateRows = 'repeat(16,1fr)';
-    console.log(sketchBoard);
+    sketchBoard.style.gridTemplateColumns = `repeat(${size},1fr)`;
+    sketchBoard.style.gridTemplateRows = `repeat(${size},1fr)`;
 
+        for (i=0;i<256;i++){
+            let square = document.createElement('div');
+            square.style.backgroundColor="blue";
+            sketchBoard.insertAdjacentElement('beforeend',square);
+        }
+    } 
+        
+porpulateBoard(16);
